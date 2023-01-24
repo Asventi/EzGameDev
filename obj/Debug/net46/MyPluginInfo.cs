50385603c3d6d7b5fb5c203e0eebfb7ff49b4e7e
Asventi <84627690+asventi@users.noreply.github.com>
2023-01-25 00:45:11 +0100

    First commit

---
+++ obj/Debug/net46/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace EzGameDev
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "EzGameDev";
+        public const string PLUGIN_NAME = "EzGameDev";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
